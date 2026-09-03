b9a4c48ef75e1e5b67ce735cc95390b87f279c37
mohit <mohitgiri1103@gmail.com>
2026-09-03 20:23:26 +0530

    add endpoint to fetch categories

---
+++ app/api/categories/route.ts
@@ -0,0 +1,39 @@
+import prisma from "@/lib/prisma"
+import { NextResponse } from "next/server"
+
+
+export async function GET() {
+    try {
+        const categories = await prisma.category.findMany({
+            where: {
+                products: {
+                    some: { isActive: true }
+                }
+            },
+            orderBy: {
+                name: "asc",
+            },
+            select: {
+                id: true,
+                name: true,
+                slug: true,
+                icon: true,
+            }
+        });
+        return NextResponse.json({
+            success: true,
+            data: categories,
+        });
+
+    } catch (error) {
+        console.error("Error while fetching categories: ", error);
+        return NextResponse.json({
+            success: false,
+            message: "Failed to fetch categories",
+        },
+            {
+                status: 500
+            }
+        );
+    }
+}
